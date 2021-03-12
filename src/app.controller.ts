57f281bf1d921699053676f132742aec4152ed2d
Alexander Esekhile <izebhijie.esekhile@pau.edu.ng>
2021-03-12 18:59:02 +0100

    done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
+import { Controller, Get, Render } from '@nestjs/common'; import { AppService } from './app.service';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
+  constructor(private readonly appService: AppService) { }
+  @Get() @Render('home.html') getHome(): {} {
+    return this.appService.getHome();
+  }
+  @Get('about-us') @Render('about-us.html') getAboutUs(): {} {
+    return this.appService.getAboutUs();
+  }
+  @Get('hello') getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('hello2') @Render('index.html') getHello2(): {} {
+    return this.appService.getHello2();
   }
 }
